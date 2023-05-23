137591e69a26183f7cf8e4a7acbdd20053262683
pipuf <ceciliapaolucci@hotmail.com>
2023-05-23 10:53:28 -0300

    menu dinamico

--- assets/vue/recetasMenu.js
+++ assets/vue/recetasMenu.js
@@ -8,53 +8,51 @@
             data: function () {
                 return {
                     /*variables de inicializacion*/
-                    meals: null,
-                    receta: null,
+                    mealList: null,
+                    mealDetails: []
                 };
             },
             mounted: function() {
                 this.handlerRecetasMenu() 
             },
             methods: {
+                getMealDetails: async function (mealID) {
+                    try {
+                        // Trae todas las recetas de la categoría Seafood
+                        let response = await fetch(URL_API+"lookup.php?i="+mealID)
+                        let data = await response.json();
+                        let mealDetalles = data.meals;
+                        let mealsLength = this.mealDetails.push(mealDetalles.strInstructions)                          
+                        return mealsLength
+                    } catch (error) {
+                        console.log({ error });
+                    }
+                }, 
+                               
                 handlerRecetasMenu: async function () {
                     try {
+                        // Trae todas las recetas de la categoría Seafood
                         let response = await fetch(URL_API+"filter.php?c=Seafood");
                         let data = await response.json();
-                        let datos=data.meals[0];
-                        this.receta= "<div>{{meals.strMeal}}</div>"
-                        // this.receta = "<div class='menu-card animation-flip'>"
-                        // this.receta = this.receta + "<div class='menu-card-content animation-flip-content'>"
-                        // this.receta = this.receta + "<div class='menu-card-front'>"
-                        // this.receta = this.receta + "<h4 class='titulo-menu-card'>{{meals.strMeal}}</h4>"
-                        // this.receta = this.receta + "<img img :src=datos.strMealThumb alt='' class='img-menu-card'>"
-                        // this.receta = this.receta + "</div>"
-                        // this.receta = this.receta + "<div class='menu-card-info animation-flip-info'>"
-                        // this.receta = this.receta + "h4 class='titulo-menu-card'>{{meals.strMeal}}</h4>"
-                        // this.receta = this.receta + "<p class='menu-card-info-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem possimus corrupti voluptates, nobis suscipit ut modi hic vel totam repudiandae!</p>"
-                        // this.receta = this.receta + "</div>"
-                        // this.receta = this.receta + "</div>"
-                        // this.receta = this.receta + "</div>"
-                       
-                        
-                        
-                        
-                        
-                        // console.log(this.nombre)
-                        // this.img = data.meals[0].strMealThumb;
-                        // for (var i=1; i<21; i++){ 
-                        //     if (data.meals[0]["strIngredient"+i] != null)
-                        //         if (data.meals[0]["strIngredient"+i].trim()!="")
-                        //             this.ingredientes[i-1]=data.meals[0]["strIngredient"+i].trim() +" "+data.meals[0]["strMeasure"+i].trim()
-                        //         else
-                        //             this.ingredientes[i-1]=""
-                        // }
-                        // console.log(this.ingredientes)
-                        // this.preparacion=data.meals[0].strInstructions
+                        this.mealList = data.meals;
+
+                        let mealsLength = 0
+                        console.log(this.mealList)
+                        this.mealList.forEach(meal => {
+                            let mealsLength=this.getMealDetails(meal.idMeal)  
+                            console.log(mealsLength)                       
+                        });
+                        console.log(mealsLength)
+                        console.log(this.mealDetails)
 
                     } catch (error) {
                         console.log({ error });
                     }
                 },
+
+                selectHandler: function (index, meal) {
+                    this.activeItem = index;
+                },                
             },
         };
 
