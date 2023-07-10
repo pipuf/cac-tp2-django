538c067bed5f600e62283bfa5dc297ad8803fa05
pipuf <ceciliapaolucci@hotmail.com>
2023-07-10 13:09:46 -0300

    Update allowed hosts y prod DB config

--- naceluja/settings.py
+++ naceluja/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hi^^-wsblmxd@+1mjcxhmea+imhhxbf0moyg6imdzf1k&0pm!e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['pipuf.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','pipuf.pythonanywhere.com']
 
 
 # Application definition
